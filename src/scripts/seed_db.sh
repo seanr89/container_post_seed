14a9f927e2a18b2581136784f32b881b7af5e37c
seanr89 <srafferty89@gmail.com>
2024-04-13 14:43:32 +0100

    Refactor seed_db.sh script to use environment variables for database connection details

--- src/scripts/seed_db.sh
+++ src/scripts/seed_db.sh
@@ -7,7 +7,7 @@ dbhost="localhost"
 dbuser="flixieuser"
 dbpass="D0ugasaur"
 
-echo "Executing script!"
+echo "Seeding database with data..."
 
 export PGPASSWORD=$dbpass
 # psql -U $dbuser -d $dbname -c "SELECT c_defaults  FROM user_info WHERE c_uid = 'testuser'"
@@ -17,6 +17,18 @@ export PGPASSWORD=$dbpass
 #   -h $hostname -d $dbname -U $dbuser \
 #   -c "\copy mytable (column1, column2)  from '/path/to/local/file.csv' with delimiter as ','"
 
+echo "Seeding Colours"
 psql -h $dbhost -d $dbname -U $dbuser -f ./files/_colour.sql
 
-echo "App Complete!"
+echo "Seeding Countries"
+psql -h $dbhost -d $dbname -U $dbuser -f ./files/_country.sql
+
+
+echo "Seeding Image Types"
+psql -h $dbhost -d $dbname -U $dbuser -f ./files/_imagetype.sql
+
+
+echo "Seeding Languages"
+psql -h $dbhost -d $dbname -U $dbuser -f ./files/_language.sql
+
+echo "Seeding Complete!"
