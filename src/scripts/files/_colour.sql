bf6309083c9b490ef54d660a127a99a332e34688
seanr89 <srafferty89@gmail.com>
2024-04-13 13:34:07 +0100

    Update seed_db.sh script to use environment variables for database connection details

---
+++ src/scripts/files/_colour.sql
@@ -0,0 +1,19 @@
+INSERT INTO util."Color" (hex_code,name) VALUES
+	 ('#FF7F50','Coral'),
+	 ('#4B0082','Indigo'),
+	 ('#50C878','Emerald Green'),
+	 ('#FFA500','Fiery Orange'),
+	 ('#39FF14','Neon Green'),
+	 ('#8A2BE2','Deep Purple'),
+	 ('#FF0000','Cherry Red'),
+	 ('#4169E1','Royal Blue'),
+	 ('#DAA520','Goldenrod'),
+	 ('#FF00FF','Magenta');
+INSERT INTO util."Color" (hex_code,name) VALUES
+	 ('#40E0D0','Turquoise'),
+	 ('#008080','Teal'),
+	 ('#FFDAB9','Peach'),
+	 ('#800000','Maroon'),
+	 ('#708090','Slate'),
+	 ('#D8BFD8','Lavender'),
+	 ('#87CEEB','Sky Blue');
--- src/scripts/seed_db.sh
+++ src/scripts/seed_db.sh
@@ -2,8 +2,21 @@
 
 set -e -u #Exit immediately if a command exits with a non-zero status.
 
+dbname="postgres"
+dbhost="localhost"
+dbuser="flixieuser"
+dbpass="D0ugasaur"
+
 echo "Executing script!"
 
-psql -U postgres -d database_name -c "SELECT c_defaults  FROM user_info WHERE c_uid = 'testuser'"
+export PGPASSWORD=$dbpass
+# psql -U $dbuser -d $dbname -c "SELECT c_defaults  FROM user_info WHERE c_uid = 'testuser'"
+
+## File based approach
+# psql \
+#   -h $hostname -d $dbname -U $dbuser \
+#   -c "\copy mytable (column1, column2)  from '/path/to/local/file.csv' with delimiter as ','"
+
+psql -h $dbhost -d $dbname -U $dbuser -f ./files/_colour.sql
 
 echo "App Complete!"
